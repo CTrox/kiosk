f36ef6dd0986d7a817e92afa624ef3534bd31493
Cyrill Troxler <cyrilltroxler@gmail.com>
2017-09-12 19:16:41 +0200

    new intellij project

---
+++ src/ch/ctrox/school/kiosk/Main.java
@@ -0,0 +1,12 @@
+package ch.ctrox.school.kiosk;
+
+/**
+ * @author Cyrill Troxler <cyrilltroxler@gmail.com>
+ * @since 12/09/17
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
